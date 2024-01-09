ebd02a7a6add88aac7cc396278643e351a4ee6d9
Kris <kris.aubuchon@discourse.org>
2024-01-09 18:30:15 -0500

    DEV: update localStorage behavior (#2)

--- javascripts/discourse/services/animation-event.js
+++ javascripts/discourse/services/animation-event.js
@@ -4,6 +4,24 @@ import Service from "@ember/service";
 
 export default class animationEvent extends Service {
   @tracked startAnimation = false;
+  currentTime = new Date().getTime();
+
+  storageExpired(objectName) {
+    const data = JSON.parse(localStorage.getItem(objectName));
+    if (!data) {
+      return true;
+    }
+    return this.currentTime > data.expiresAt;
+  }
+
+  setLocalStorage(objectName) {
+    const expiryTime = 24 * 60 * 60 * 1000; // 24 hours
+    const data = {
+      timestamp: this.currentTime,
+      expiresAt: this.currentTime + expiryTime,
+    };
+    localStorage.setItem(objectName, JSON.stringify(data));
+  }
 
   @action
   handleActionToggled() {
